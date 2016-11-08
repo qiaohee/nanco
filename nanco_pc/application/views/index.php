130c4c5933c7638dec30169d884f6d794918f7e8
perfect <449725124@qq.com>
2016-11-08 16:02:21 +0800

    ok

--- nanco_pc/application/views/index.php
+++ nanco_pc/application/views/index.php
@@ -4,49 +4,22 @@
 	<div class="container">
 		<div class="row">
 			<!-- start of sidebar -->
-			<aside class="span3 page-sidebar">
-
-				<section class="widget">
-					<div class="support-widget">
-						<h3 class="title">Support</h3>
-						<p class="intro">Need more support? If you did not found an answer, contact us for further help.</p>
-					</div>
-				</section>
-
+			<aside class="span2 page-sidebar">
 				<section class="widget">
 					<div class="quick-links-widget">
-						<h3 class="title">Quick Links</h3>
+						<h3 class="title">全部教程</h3>
 						<ul id="menu-quick-links" class="menu clearfix">
-							<li><a href="index-2.html">Home</a></li>
-							<li><a href="articles-list.html">Articles List</a></li>
-							<li><a href="faq.html">FAQs</a></li>
-							<li><a href="contact.html">Contact</a></li>
+							<li><a href="index-2.html">HTML / CSS</a></li>
+							<li><a href="articles-list.html">JavaScript</a></li>
+							<li><a href="faq.html">服务端</a></li>
+							<li><a href="contact.html">数据库</a></li>
+							<li><a href="contact.html">移动端</a></li>
+							<li><a href="contact.html">XML 教程</a></li>
+							<li><a href="contact.html">ASP.NET</a></li>
+							<li><a href="contact.html">Web Services</a></li>
+							<li><a href="contact.html">开发工具</a></li>
+							<li><a href="contact.html">网站建设</a></li>
 						</ul>
-					</div>
-				</section>
-
-				<section class="widget">
-					<h3 class="title">Tags</h3>
-					<div class="tagcloud">
-						<a href="#" class="btn btn-mini">basic</a>
-						<a href="#" class="btn btn-mini">beginner</a>
-						<a href="#" class="btn btn-mini">blogging</a>
-						<a href="#" class="btn btn-mini">colour</a>
-						<a href="#" class="btn btn-mini">css</a>
-						<a href="#" class="btn btn-mini">date</a>
-						<a href="#" class="btn btn-mini">design</a>
-						<a href="#" class="btn btn-mini">files</a>
-						<a href="#" class="btn btn-mini">format</a>
-						<a href="#" class="btn btn-mini">header</a>
-						<a href="#" class="btn btn-mini">images</a>
-						<a href="#" class="btn btn-mini">plugins</a>
-						<a href="#" class="btn btn-mini">setting</a>
-						<a href="#" class="btn btn-mini">templates</a>
-						<a href="#" class="btn btn-mini">theme</a>
-						<a href="#" class="btn btn-mini">time</a>
-						<a href="#" class="btn btn-mini">videos</a>
-						<a href="#" class="btn btn-mini">website</a>
-						<a href="#" class="btn btn-mini">wordpress</a>
 					</div>
 				</section>
 
@@ -54,10 +27,10 @@
 			<!-- end of sidebar -->
 
 			<!-- start of page content -->
-			<div class="span9 page-content">
+			<div class="span10 page-content">
 
 				<div class="row home-category-list-area">
-					<div class="span8">
+					<div class="span10">
 						<h2>HTML / CSS</h2>
 					</div>
 				</div>
@@ -125,7 +98,7 @@
 				</div>
 
 				<div class="row home-category-list-area">
-					<div class="span12">
+					<div class="span10">
 						<h2>HTML / CSS</h2>
 					</div>
 				</div>
