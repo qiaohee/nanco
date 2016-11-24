e22e9d4622611f9225c23c17ab937c747b13f2da
perfect <449725124@qq.com>
2016-11-24 10:22:23 +0800

    ok

--- nanco_pc/application/controllers/main.php
+++ nanco_pc/application/controllers/main.php
@@ -20,6 +20,35 @@ class main extends CI_Controller {
 	 */
 	public function index()
 	{
+		//连接本地的 Redis 服务
+		$redis = new Redis();
+		$redis->connect('127.0.0.1', 6379);
+		echo "Connection to server sucessfully";
+
+		header("content-type:text/html;charset=utf-8");
+		$mywatchkey = $redis->get("mywatchkey");
+		$rob_total = 100;   //抢购数量
+		if($mywatchkey<$rob_total){
+			$redis->watch("mywatchkey");
+			$redis->multi();
+
+			//设置延迟，方便测试效果。
+			sleep(5);
+			//插入抢购数据
+			$redis->hSet("mywatchlist","user_id_".mt_rand(1, 9999),time());
+			$redis->set("mywatchkey",$mywatchkey+1);
+			$rob_result = $redis->exec();
+			if($rob_result){
+				$mywatchlist = $redis->hGetAll("mywatchlist");
+				echo "抢购成功！<br/>";
+				echo "剩余数量：".($rob_total-$mywatchkey-1)."<br/>";
+				echo "用户列表：<pre>";
+				var_dump($mywatchlist);
+			}else{
+				echo "手气不好，再抢购！";exit;
+			}
+		}
+		exit;
 		$this->load->view('header');
 		$this->load->view('index');
 		$this->load->view('footer');
