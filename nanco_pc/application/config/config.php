f7d8e4d2a2b62a6ba4877bc534dcfcf6d97d828f
perfect <449725124@qq.com>
2016-12-01 12:23:55 +0800

    ok

--- nanco_pc/application/config/config.php
+++ nanco_pc/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://www.w3crun.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
